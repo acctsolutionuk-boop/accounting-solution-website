2e11026d9b4422f13091c26ec4c0a152766f212b
acctsolutionuk-boop <acctsolutionuk@gmail.com>
2026-06-03 15:06:32 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -13,14 +13,3 @@ document.querySelectorAll('.mobile-menu a').forEach(link => {
     mobileMenu.classList.remove('open');
   });
 });
-
-// Tawk.to
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6a08429ac744531c43731f2e/1joo48bfl';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
